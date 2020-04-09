586d3635545b17a69ecca0b8583efb59e3ef4de2
tesla lee <leechee89@gmail.com>
2020-04-09 23:17:19 +0800

    restart

--- init.el
+++ init.el
@@ -5,37 +5,68 @@
 ;; You may delete these explanatory comments.
 ;; (package-initialize)
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(load "~/.emacs.d/init-packages")
+;; MELPA
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  )
+(package-initialize)
 
-(require 'init-utils)
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;; (load "~/.emacs.d/init-packages")
+
+;; (require 'init-utils)
 
 (require 'init-elpa)
-(require 'init-editor)
-(require 'init-themes)
-(require 'init-fonts)
+;; (require 'init-editor)
+;; (require 'init-themes)
+;; (require 'init-fonts)
 ;; (require 'init-mmmm)
 
-(require 'init-ivy)
-(require 'init-ido)
-(require 'init-projectile)
-(require 'init-rails)
-(require 'init-spaceline)
-(require 'init-auto-complete)
-(require 'init-smooth-scrolling)
+;; (require 'init-ivy)
+;; (require 'init-ido)
+;; (require 'init-projectile)
+;; (require 'init-rails)
+;; (require 'init-spaceline)
+;; (require 'init-auto-complete)
+;; (require 'init-smooth-scrolling)
 
 ;;
 ;; (require 'init-ruby-mode)
 
 ;;=======
 
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(initial-frame-alist (quote ((fullscreen . maximized))))
+;;  '(package-selected-packages (quote (solarized-theme auto-complete))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages (quote (solarized-theme auto-complete))))
+ '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
