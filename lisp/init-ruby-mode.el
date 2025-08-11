1b841d2b60213676b273c82b4b160727c35254f0
Tesla Lee <leechee89@gmail.com>
2025-08-11 21:53:00 +0800

    add(package): ruby mode

--- lisp/init-ruby-mode.el
+++ lisp/init-ruby-mode.el
@@ -1,137 +1,42 @@
-;;; Basic ruby setup
-(require-package 'ruby-mode)
-(require-package 'ruby-hash-syntax)
+;;; init-ruby-mode.el --- Ruby mode enhancements
 
-(add-auto-mode 'ruby-mode
-               "Rakefile\\'" "\\.rake\\'" "\\.rxml\\'"
-               "\\.rjs\\'" "\\.irbrc\\'" "\\.pryrc\\'" "\\.builder\\'" "\\.ru\\'"
-               "\\.gemspec\\'" "Gemfile\\'" "Kirkfile\\'")
+;; Enable Enhanced Ruby Mode for *.rb, Rakefiles, etc.
+(use-package enh-ruby-mode
+  :ensure t
+  :mode (("\\.\\(rb\\|rake\\|ru\\|gemspec\\)\\'" . enh-ruby-mode))
+  :interpreter ("ruby" . enh-ruby-mode)
+  :hook (enh-ruby-mode . (lambda ()
+                           ;; Use 2 spaces for indentation
+                           (setq-local ruby-indent-level 2
+                                       indent-tabs-mode nil)
+                           ;; Auto-pair braces, parens, quotes
+                           (electric-pair-local-mode 1))))
 
-(add-auto-mode 'conf-mode "Gemfile\\.lock'")
+;; LSP support for Ruby via Solargraph
+(use-package lsp-mode
+  :ensure t
+  :hook ((enh-ruby-mode ruby-mode) . lsp-deferred)
+  :commands (lsp lsp-deferred)
+  :init
+  ;; prefix for lsp-command-keymap (few alternatives: "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  ;; use bundler to start solargraph if Gemfile present
+  (setq lsp-solargraph-use-bundler t
+        ;; format on save
+        lsp-solargraph-autoformat t
+        ;; show diagnostics from server
+        lsp-solargraph-diagnostics t))
 
-(setq-default
- ruby-use-encoding-map nil
- ruby-insert-encoding-magic-comment nil)
-
-(after-load 'ruby-mode
-  (define-key ruby-mode-map (kbd "TAB") 'indent-for-tab-command)
-
-  ;; Stupidly the non-bundled ruby-mode isn't a derived mode of
-  ;; prog-mode: we run the latter's hooks anyway in that case.
-  (add-hook 'ruby-mode-hook
-            (lambda ()
-              (unless (derived-mode-p 'prog-mode)
-                (run-hooks 'prog-mode-hook)))))
-
-(add-hook 'ruby-mode-hook 'subword-mode)
-
-(after-load 'page-break-lines
-  (push 'ruby-mode page-break-lines-modes))
-
-(require-package 'rspec-mode)
-
-;;; Inferior ruby
-(require-package 'inf-ruby)
-
-
-
-;;; Ruby compilation
-(require-package 'ruby-compilation)
-
-(after-load 'ruby-mode
-  (let ((m ruby-mode-map))
-    (define-key m [S-f7] 'ruby-compilation-this-buffer)
-    (define-key m [f7] 'ruby-compilation-this-test)))
-
-(after-load 'ruby-compilation
-  (defalias 'rake 'ruby-compilation-rake))
-
-
-;;; Robe
-(when (maybe-require-package 'robe)
-  (after-load 'ruby-mode
-    (add-hook 'ruby-mode-hook 'robe-mode))
-  (after-load 'company
-    (dolist (hook '(ruby-mode-hook inf-ruby-mode-hook html-erb-mode-hook haml-mode))
-      (add-hook hook
-                (lambda () (sanityinc/local-push-company-backend 'company-robe))))))
-
-
-
-;; Customise highlight-symbol to not highlight do/end/class/def etc.
-(defun sanityinc/suppress-ruby-mode-keyword-highlights ()
-  "Suppress highlight-symbol for do/end etc."
-  (set (make-local-variable 'highlight-symbol-ignore-list)
-       (list (concat "\\_<" (regexp-opt '("do" "end")) "\\_>"))))
-(add-hook 'ruby-mode-hook 'sanityinc/suppress-ruby-mode-keyword-highlights)
-
-
-;;; ri support
-(require-package 'yari)
-(defalias 'ri 'yari)
-
-
-
-(require-package 'goto-gem)
-
-
-(require-package 'bundler)
-
-
-;;; YAML
-
-(when (maybe-require-package 'yaml-mode)
-  (add-auto-mode 'yaml-mode "\\.yml\\.erb\\'"))
-
-
-
-;;; ERB
-(require-package 'mmm-mode)
-(defun sanityinc/ensure-mmm-erb-loaded ()
-  (require 'mmm-erb))
-
-(require 'derived)
-
-(defun sanityinc/set-up-mode-for-erb (mode)
-  (add-hook (derived-mode-hook-name mode) 'sanityinc/ensure-mmm-erb-loaded)
-  (mmm-add-mode-ext-class mode "\\.erb\\'" 'erb))
-
-(let ((html-erb-modes '(html-mode html-erb-mode nxml-mode)))
-  (dolist (mode html-erb-modes)
-    (sanityinc/set-up-mode-for-erb mode)
-    (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-js)
-    (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-css)))
-
-(mapc 'sanityinc/set-up-mode-for-erb
-      '(coffee-mode js-mode js2-mode js3-mode markdown-mode textile-mode))
-
-(mmm-add-mode-ext-class 'html-erb-mode "\\.jst\\.ejs\\'" 'ejs)
-
-(add-auto-mode 'html-erb-mode "\\.rhtml\\'" "\\.html\\.erb\\'")
-(add-to-list 'auto-mode-alist '("\\.jst\\.ejs\\'"  . html-erb-mode))
-(mmm-add-mode-ext-class 'yaml-mode "\\.yaml\\(\\.erb\\)?\\'" 'erb)
-
-(dolist (mode (list 'js-mode 'js2-mode 'js3-mode))
-  (mmm-add-mode-ext-class mode "\\.js\\.erb\\'" 'erb))
-
-
-;;----------------------------------------------------------------------------
-;; Ruby - my convention for heredocs containing SQL
-
-;; (eval-after-load 'mmm-mode
-;;   '(progn
-;;      (mmm-add-classes
-;;       '((ruby-heredoc-sql
-;;          :submode sql-mode
-;;          :front "<<-?[\'\"]?\\(end_sql\\)[\'\"]?"
-;;          :save-matches 1
-;;          :front-offset (end-of-line 1)
-;;          :back "^[ \t]*~1$"
-;;          :delimiter-mode nil)))
-;;      (mmm-add-mode-ext-class 'ruby-mode "\\.rb\\'" 'ruby-heredoc-sql)))
-
-;(add-to-list 'mmm-set-file-name-for-modes 'ruby-mode)
-
-
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :after lsp-mode
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (setq lsp-ui-doc-enable t
+        lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-hover t))
 
 (provide 'init-ruby-mode)
+;;; init-ruby-mode.el ends here
